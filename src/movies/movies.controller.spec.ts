947a3649bcae7ca6e9b7eb06691fe4a5438aaefd
Nina <niculina.serban@gmail.com>
2021-10-01 10:23:58 +0300

    bugfix tests

--- src/movies/movies.controller.spec.ts
+++ src/movies/movies.controller.spec.ts
@@ -1,63 +1,104 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
-import { HttpModule, HttpService } from '@nestjs/axios';
 import { Movie } from './entities/movie.entity';
-import { MovieRepository } from './movie.repository';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { REQUEST } from '@nestjs/core';
+import { MovieAPI } from './movieAPI';
+import { MovieDto } from './dto/movie.dto';
 
 describe('MoviesController', () => {
-  let httpService: HttpService;
   let movieService: MoviesService;
-  let controller: MoviesController;
-  let movieRepository;
-  const mockMovieRepository = () => ({
-    save: jest.fn(),
-    getAll: jest.fn()
+  let movieController: MoviesController;
+  const mockMovieService = () => ({
+    create: jest.fn(),
+    getAll: jest.fn(),
+    countAll: jest.fn(),
+    fetch: jest.fn()
   });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports:[HttpModule],
-      controllers: [MoviesController],
+      imports:[],
       providers: [
-        MoviesService,
+        MoviesController,
         {
-          provide: MovieRepository,
-          useFactory: mockMovieRepository,
+          provide: MoviesService,
+          useFactory: mockMovieService,
+        },
+        {
+          provide: REQUEST,
+          useValue: {
+             user: {
+              "userId": 123,
+              "name": "Basic Thomas",
+              "role": "basic",
+              "iat": 1606221838,
+              "exp": 1606223638,
+              "iss": "https://www.netguru.com/",
+              "sub": "123"
+            },
+          },
         },
       ],
     }).compile();
 
-    controller = module.get<MoviesController>(MoviesController);
+    movieController = module.get<MoviesController>(MoviesController);
     movieService = await module.get<MoviesService>(MoviesService);
-    movieRepository = await module.get<MovieRepository>(MovieRepository);
-    httpService = await module.get<HttpService>(HttpService);
   });
 
   describe('postMovie', () => {
     it('should save a movie in the database', async () => {
-      const movie:Movie = {
+      const movieFromOmdbApi: MovieAPI = {
+        Title: "title",
+        Released: new Date("1994-09-21T21:00:00.000Z"),
+        Genre: "genre",
+        Director: "director"
+      }
+      Object(movieService.fetch).mockResolvedValue(movieFromOmdbApi);
+
+      const savedMovie:Movie = {
         id: 1,
         title: 'Friends',
         released: new Date('1994-09-21T21:00:00.000Z'),
         genre: 'Comedy, Romance',
         director: "N/A",
-        userId: 123
+        userId: 123,
+        created_at: new Date("2021-10-01")
       };
+      Object(movieService.create).mockResolvedValue(savedMovie);
 
-      movieRepository.save.mockResolvedValue(movie);
-      expect(movieRepository.save).not.toHaveBeenCalled();
-
-      const result = await movieService.create(movie);
-      expect(movieRepository.save).toHaveBeenCalledWith(
-        movie,
-      );
-      expect(result).toEqual(movie);
+      const createDto: CreateMovieDto = {
+        title: 'Friends'
+      }
+      const result = await movieController.create(createDto);
+      
+      const movieDto: MovieDto = {
+        id: 1,
+        title: 'Friends',
+        released: new Date('1994-09-21T21:00:00.000Z'),
+        genre: 'Comedy, Romance',
+        director: "N/A",
+        userId: 123,
+      }
+      expect(result).toEqual(movieDto);
     });
   });
 
-
-  // it('should be defined', () => {
-  //   expect(controller).toBeDefined();
-  // });
+  describe('getMovies', () => {
+    const savedMovie:Movie = {
+      id: 1,
+      title: 'Friends',
+      released: new Date('1994-09-21T21:00:00.000Z'),
+      genre: 'Comedy, Romance',
+      director: "N/A",
+      userId: 123,
+      created_at: new Date("2021-10-01")
+    };
+    it('should get all movies', async () => {
+      Object(movieService.getAll).mockResolvedValue(savedMovie);
+      const result = await movieService.getAll(savedMovie.userId);
+      expect(result).toEqual(savedMovie);
+    });
+  });
 });
